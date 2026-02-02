c5dd3ff4573ec93af4eed6839f0ec00e26adb3db
Niraj44444 <basantakhadka44664466@gmail.com>
2026-02-02 15:21:06 +0000

    dd

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,8 +1,14 @@
 import './App.css'
+import Buttons from './Buttons.jsx' 
 import Profile from './Profile.jsx'
 export default function App() {
   return (
     <main>
+      React ⚛️ + Vite ⚡ + Replit!
+      <br />
+      {/* This is how you show the button */}
+      <Buttons /> 
+      <br />
       {/* This is how you show the profile */}
       <Profile />
     </main>
